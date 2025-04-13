86410a03da45a0bc9f6754fb67057ab68d3f4239
Rafael Escaleira <rafael.escaleira@g.globo>
2025-04-13 18:26:33 -0400

    Add array extension to entity

--- Sources/RefdsFoundation/Entity/RefdsEntity.swift
+++ Sources/RefdsFoundation/Entity/RefdsEntity.swift
@@ -15,5 +15,46 @@ public protocol RefdsEntity:
     CustomStringConvertible,
     Sendable,
     RefdsLogger {
+}
+
+extension RefdsEntity {
+    public var logger: Logger {
+        Logger(
+            subsystem: Bundle.main.bundleIdentifier ?? String(describing: Self.self),
+            category: String(describing: Self.self)
+        )
+    }
     
+    public func log() {
+        do {
+            let content = try json
+            logger.info("\(content)")
+        } catch {
+            logger.error("\(error.localizedDescription)")
+        }
+    }
+}
+
+extension Array: RefdsEntity where Element: RefdsEntity {}
+
+extension Array: @retroactive Identifiable where Element: RefdsEntity {
+    public var id: UUID { .init() }
+}
+
+extension Array: RefdsLogger where Element: RefdsEntity {
+    public var logger: Logger {
+        Logger(
+            subsystem: Bundle.main.bundleIdentifier ?? String(describing: Element.self),
+            category: String(describing: Element.self)
+        )
+    }
+    
+    public func log() {
+        do {
+            let content = try json
+            logger.info("\(content)")
+        } catch {
+            logger.error("\(error.localizedDescription)")
+        }
+    }
 }
