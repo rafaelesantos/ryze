bff640152ef3255719e8681e4672441cc764ce86
Rafael Escaleira <rafael.escaleira@g.globo>
2025-04-13 14:38:59 -0400

    Add endpoint structure

---
+++ Sources/RefdsNetwork/Endpoint/RefdsNetworkMethod.swift
@@ -0,0 +1,14 @@
+//
+//  RefdsNetworkMethod.swift
+//  Refds
+//
+//  Created by Rafael Escaleira on 28/03/25.
+//
+
+public enum RefdsNetworkMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
