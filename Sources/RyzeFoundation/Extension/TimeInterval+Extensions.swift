5d85ed6b7891492fecce980faa914a1334f89348
Rafael Santos <rafaelescaleira@icloud.com>
2025-07-14 15:06:10 -0400

    Add some extensions to time interval

---
+++ Sources/RyzeFoundation/Extension/TimeInterval+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  TimeInterval+Extensions.swift
+//  Ryze
+//
+//  Created by Rafael Escaleira on 14/07/25.
+//
+
+@_exported import Foundation
+
+public extension TimeInterval {
+    var seconds: TimeInterval { self }
+    var minutes: TimeInterval { self * 60 }
+    var hours: TimeInterval { self * 3600 }
+    var days: TimeInterval { self * 86400 }
+}
