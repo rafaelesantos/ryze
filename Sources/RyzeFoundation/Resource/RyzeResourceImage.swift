3d8a5a53853d6f65906b897941aed2e8160ab1e0
Rafael Santos <rafaelescaleira@icloud.com>
2025-07-14 08:47:56 -0400

    Add image resource

---
+++ Sources/RyzeFoundation/Resource/RyzeResourceImage.swift
@@ -0,0 +1,12 @@
+//
+//  RyzeResourceImage.swift
+//  Ryze
+//
+//  Created by Rafael Escaleira on 14/07/25.
+//
+
+import SwiftUI
+
+public protocol RyzeResourceImage {
+    var image: Image { get }
+}
