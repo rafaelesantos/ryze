7a3e3eea67241d1b56e90a1c1961dde962e6b955
Rafael Santos <rafaelescaleira@icloud.com>
2025-07-22 18:26:29 -0400

    Update video view

--- Sources/RyzeVideo/RyzeVideoView.swift
+++ Sources/RyzeVideo/RyzeVideoView.swift
@@ -17,22 +17,19 @@ public struct RyzeVideoView: View {
     }
     
     public var body: some View {
-        ZStack {
-            GeometryReader { proxy in
-                if let player {
-                    RyzeVideoPlayer(player: player)
-                        .frame(width: proxy.size.width, height: proxy.size.width * (9 / 16))
-                }
-            }
+        playerView
+            .onChange(of: url) { updatePlayer() }
+    }
+    
+    @ViewBuilder
+    var playerView: some View {
+        if let player {
+            RyzeVideoPlayer(player: player)
         }
-        .onChange(of: url) {
-            guard let url else {
-                return player = nil
-            }
-            player = AVPlayer(url: url)
-        }
-        .onDisappear {
-            player?.pause()
-        }
+    }
+    
+    func updatePlayer() {
+        guard let url else { return player = nil }
+        player = AVPlayer(url: url)
     }
 }
