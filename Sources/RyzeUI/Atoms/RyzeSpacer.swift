1b839c7bb151cd0716e2ecb1d0559fddadd218e4
Rafael Santos <rafaelescaleira@icloud.com>
2025-08-01 14:20:49 -0400

    Create spacer component

---
+++ Sources/RyzeUI/Atoms/RyzeSpacer.swift
@@ -0,0 +1,31 @@
+//
+//  RyzeSpacer.swift
+//  Ryze
+//
+//  Created by Rafael Escaleira on 01/08/25.
+//
+
+@_exported import SwiftUI
+@_exported import RyzeFoundation
+
+public struct RyzeSpacer: RyzeView {
+    @Environment(\.theme) var theme
+    
+    let size: RyzeSpacing?
+    
+    public init(size: RyzeSpacing? = .zero) {
+        self.size = size
+    }
+    
+    public var body: some View {
+        Spacer(minLength: size?.rawValue(for: theme.spacing))
+    }
+    
+    public static var mock: some View {
+        RyzeSpacer()
+    }
+}
+
+#Preview {
+    RyzeSymbol.mock
+}
