9e679cdffdaffb30ef3012d5259ead38d3b6b717
Rafael Santos <rafaelescaleira@icloud.com>
2025-08-08 00:22:32 -0400

    Update theme color protocol

--- Sources/RyzeUI/Protocols/RyzeColorProtocol.swift
+++ Sources/RyzeUI/Protocols/RyzeColorProtocol.swift
@@ -8,22 +8,22 @@
 @_exported import SwiftUI
 
 public protocol RyzeColorProtocol: Sendable {
-    var primary: Color { get }
-    var secondary: Color { get }
-    var background: Color { get }
-    var backgroundSecondary: Color { get }
-    var shadow: Color { get }
-    var surface: Color { get }
-    var text: Color { get }
-    var textSecondary: Color { get }
-    var border: Color { get }
-    var error: Color { get }
-    var success: Color { get }
-    var warning: Color { get }
-    var info: Color { get }
-    var disabled: Color { get }
-    var hover: Color { get }
-    var pressed: Color { get }
-    var white: Color { get }
-    var black: Color { get }
+    var primary: Color { get set }
+    var secondary: Color { get set }
+    var background: Color { get set }
+    var backgroundSecondary: Color { get set }
+    var shadow: Color { get set }
+    var surface: Color { get set }
+    var text: Color { get set }
+    var textSecondary: Color { get set }
+    var border: Color { get set }
+    var error: Color { get set }
+    var success: Color { get set }
+    var warning: Color { get set }
+    var info: Color { get set }
+    var disabled: Color { get set }
+    var hover: Color { get set }
+    var pressed: Color { get set }
+    var white: Color { get set }
+    var black: Color { get set }
 }
