be4a92175d67f530c65216ea006df5fad4542bd4
Escaleira <rafaelescaleira@icloud.com>
2025-09-05 08:58:13 -0400

    Add sorted by keypath

--- Sources/RyzeFoundation/Extension/Array+Extensions.swift
+++ Sources/RyzeFoundation/Extension/Array+Extensions.swift
@@ -42,3 +42,15 @@ public extension Array {
         return results
     }
 }
+
+
+public extension Sequence {
+    func sorted<T: Comparable>(
+        by keyPath: KeyPath<Element, T>,
+        using comparator: (T, T) -> Bool = (<)
+    ) -> [Element] {
+        sorted { a, b in
+            comparator(a[keyPath: keyPath], b[keyPath: keyPath])
+        }
+    }
+}
