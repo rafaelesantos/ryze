4783c12d45cd44238588fae73fb2d0435cd2687b
Rafael Escaleira <rafael.escaleira@g.globo>
2025-10-29 00:08:14 -0400

    Add chuncked

--- Sources/RyzeFoundation/Extension/Array+Extensions.swift
+++ Sources/RyzeFoundation/Extension/Array+Extensions.swift
@@ -49,6 +49,12 @@ public extension Array {
     var secondToLast: Element? {
         count > 1 ? self[count - 2] : nil
     }
+    
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
 }
 
 
